f9814233488b59e57af23151ff598768a1458dca
achachia <achachia2003@yahoo.fr>
2022-05-30 12:55:34 +0200

    commit-operateurs-base

---
+++ Operateurs-base/operateur-instanceof.java
@@ -0,0 +1,18 @@
+public class Main {
+
+   public static void main(String args[]) {
+      
+      String name = "James";
+
+      // following will return true since name is type of String
+      boolean result = name instanceof String;
+      System.out.println( result );
+   }
+}
+
+/*
+
+Output
+true
+
+*/
---
+++ Operateurs-base/operateurs-relationnels.java
@@ -0,0 +1,26 @@
+public class Main {
+
+   public static void main(String args[]) {
+      int a = 10;
+      int b = 20;
+
+      System.out.println("a == b = " + (a == b) );
+      System.out.println("a != b = " + (a != b) );
+      System.out.println("a > b = " + (a > b) );
+      System.out.println("a < b = " + (a < b) );
+      System.out.println("b >= a = " + (b >= a) );
+      System.out.println("b <= a = " + (b <= a) );
+   }
+}
+
+/*
+
+Output
+a == b = false
+a != b = true
+a > b = false
+a < b = true
+b >= a = true
+b <= a = false
+
+*/
