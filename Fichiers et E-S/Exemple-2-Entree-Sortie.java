9ee128e1e7e5ab4bdcf576daeb167abaf1209a25
achachia <achachia2003@yahoo.fr>
2022-06-16 16:51:40 +0200

    commit-E-S-v1

---
+++ Fichiers et E-S/Exemple-2-Entree-Sortie.java
@@ -0,0 +1,20 @@
+
+//Appel Package java.util
+import java.util.*;
+
+public class Main {
+   public static void main(string[] args) {
+      // Un nouvel objet scanner est instancier
+      Scanner sc = new Scanner(system.in);
+      // Affichage du premier message
+      System.out.println("Saisissez un entier : ");
+      // Affectation de l'entrée à la variable entier i
+      int i = sc.nextInt();
+      // Affichage du deuxième message
+      System.out.println("Saisissez une chaîne : ");
+      // Affectation de l'entrée à la variable chaîne str
+      String str = sc.nextLine();
+      System.out.println("FIN ! ");
+   }
+}
+
