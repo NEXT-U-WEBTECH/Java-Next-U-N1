e7c9ddf54269af28cf0cc0696198518e3ef3c88d
Docfile <achachia2003@yahoo.fr>
2022-10-12 10:22:51 +0200

    Update For-Traitement-Array.java

--- Tableaux (Array)/For-Traitement-Array.java
+++ Tableaux (Array)/For-Traitement-Array.java
@@ -9,19 +9,7 @@ public class Main {
          System.out.println(myList[i] + " ");
       }
      
-      // Summing all elements
-      double total = 0;
-      for (int i = 0; i < myList.length; i++) {
-         total += myList[i];
-      }
-      System.out.println("Total is " + total);
-      
-      // Finding the largest element
-      double max = myList[0];
-      for (int i = 1; i < myList.length; i++) {
-         if (myList[i] > max) max = myList[i];
-      }
-      System.out.println("Max is " + max);  
+     
     }
  }
 
